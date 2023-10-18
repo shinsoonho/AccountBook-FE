6cd193bf8f93c994bcb65404bceb24016f22d637
shinsoonho <tnsghek12@naver.com>
2023-10-19 00:45:38 +0900

    프로젝트 초기화

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+	<main class="container py-4">
+		<h2>Hello Vue3</h2>
+		<button class="btn btn-primary">test</button>
+	</main>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import './assets/main.css';
+
+createApp(App).mount('#app');
+
+import 'bootstrap/dist/js/bootstrap.js';
