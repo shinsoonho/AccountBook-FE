b01e01fc80bf4cda6a5f896605673f29929aa553
shinsoonho <tnsghek12@naver.com>
2023-11-13 22:14:18 +0900

    router 추가

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
 
 import './assets/main.css';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
