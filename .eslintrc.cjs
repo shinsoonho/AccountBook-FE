9a04ae13bf1e29073e4d38cd06bec6b564a749d4
shinsoonho <tnsghek12@naver.com>
2023-11-13 22:12:38 +0900

    vue/multi-word-component-names 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
 		ecmaVersion: 'latest',
 	},
 	rules: {
+		'vue/multi-word-component-names': 0,
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'prettier/prettier': [
