e74d93f7a0a66443885176bb96fdfd1b148166b1
shinsoonho <tnsghek12@naver.com>
2023-11-28 23:28:30 +0900

    api 프록시 설정

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:9999', // Spring Boot 서버 주소
+				changeOrigin: true,
+				rewrite: path => path.replace(/^\/api/, ''),
+			},
+		},
+	},
 });
