e8d92e25a50fa6a7e3fb97c32c74988c08488592
shinsoonho <tnsghek12@naver.com>
2024-04-08 23:28:39 +0900

    로그인 페이지 추가

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="wrapper-container">
+		<div class="container-top">
+			<div class="container">
+				<form action="post">
+					<label id="loginLabel" for="text">LOGIN</label>
+					<input id="loginId" type="text" placeholder="@username" />
+					<input id="loginPassword" type="password" placeholder="password" />
+					<button id="loginBtn" type="button">GO !</button>
+				</form>
+			</div>
+		</div>
+		<div class="semi-degine">∨</div>
+		<div class="container-bottom">
+			<div class="container">
+				<button id="kakaoBtn" class="kakaoBtn">Login With Kakao</button>
+				<button id="naverBtn" class="naverBtn">Login With Naver</button>
+				<button id="createBtn" class="createBtn" @click="goPage('createMember')" >
+					Create an Login
+				</button>
+				<button id="forgetBtn" class="forgetBtn">Forger Password?</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import '@/css/login.css';
+
+const router = useRouter();
+const goPage = pageName => {
+	// 페이지 이동
+	router.push({
+		name: pageName,
+	});
+};
+</script>
+<style></style>
