48e33115d9cb5f162c81fc990859d546d3a6e8c1
shinsoonho <tnsghek12@naver.com>
2024-04-08 23:28:14 +0900

    컴포넌트 정리

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '@/views/Login.vue'; // 실제 뷰 컴포넌트의 경로에 따라 수정
+import LoginPage from '@/views/LoginPage.vue'; // 실제 뷰 컴포넌트의 경로에 따라 수정
+import CreateMember from '@/views/CreateMember.vue';
 import '@/css/common.css';
 
 const routes = [
 	{
 		path: '/login',
-		component: Login,
+		name: 'login',
+		component: LoginPage,
+	},
+	{
+		path: '/login/create',
+		name: 'createMember',
+		component: CreateMember,
 	},
 ];
 
