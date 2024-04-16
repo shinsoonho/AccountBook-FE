f9de838d8c08348f6ab161d8dd6a97b55409ad51
shinsoonho <tnsghek12@naver.com>
2024-04-16 23:46:59 +0900

    메인홈 컴포넌트 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '@/views/LoginPage.vue'; // 실제 뷰 컴포넌트의 경로에 따라 수정
 import CreateMember from '@/views/CreateMember.vue';
+import MainHome from '@/views/MainHome.vue';
 import '@/css/common.css';
 
 const routes = [
+	{
+		path: '/home',
+		name: 'mainHome',
+		component: MainHome,
+	},
 	{
 		path: '/login',
 		name: 'login',
